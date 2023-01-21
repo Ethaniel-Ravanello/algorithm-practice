2923223a97877d898e0f014b349f2aac1978d8b6
Aventom <ravaneloe@gmail.com>
2023-01-21 15:53:44 +0700

    score

--- breakingRecord.js
+++ breakingRecord.js
@@ -1,3 +1,19 @@
 function breakingRecords(scores) {
-	// Write your code here
+	let highestScore = scores[0];
+	let lowestScore = scores[0];
+	let highestScoreCount = 0;
+	let lowestScoreCount = 0;
+
+	for (let i = 1; i < scores.length; i++) {
+		if (scores[i] > highestScore) {
+			highestScore = scores[i];
+			highestScoreCount++;
+		} else if (scores[i] < lowestScore) {
+			lowestScore = scores[i];
+			lowestScoreCount++;
+		}
+	}
+	return [highestScoreCount, lowestScoreCount];
 }
+
+console.log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 60, 90]));
