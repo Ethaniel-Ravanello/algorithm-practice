b81896936d207fd4bf7c3951023afc310eaac499
Aventom <ravaneloe@gmail.com>
2023-02-06 09:20:24 +0700

    belajar filter

--- firstAndLast.js
+++ firstAndLast.js
@@ -1,12 +1,20 @@
-function firstAndLast(nums, target) {
-	let result = [-1, -1];
+// function firstAndLast(nums, target) {
+// 	let result = [-1, -1];
 
-	for (let i = 0; i < nums.length; i++) {
-		if (result[0] < 0 && nums[i] === target) result[0] = i;
-		if (result[1] < 0 && nums[nums.length - i] === target)
-			result[1] = nums.length - i;
-	}
-	return result;
-}
+// 	for (let i = 0; i < nums.length; i++) {
+// 		if (result[0] < 0 && nums[i] === target) result[0] = i;
+// 		if (result[1] < 0 && nums[nums.length - i] === target)
+// 			result[1] = nums[nums.length - i];
+// 	}
+// 	console.log(result);
+// }
 
-console.log(firstAndLast([1], 1));
+// firstAndLast([1], 1);
+
+let nums = [1, 1, 2, 6, 3, 3, 3];
+
+let numsFilter = nums.filter((num) => {
+	return num !== 3;
+});
+
+console.log(numsFilter);
