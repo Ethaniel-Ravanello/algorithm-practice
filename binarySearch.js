e8de407b14165bc98e6cf7fddfda0a4fbd082e26
Aventom <ravaneloe@gmail.com>
2023-02-08 15:12:58 +0700

    reverse

--- binarySearch.js
+++ binarySearch.js
@@ -4,18 +4,15 @@
 // console.log(a === b);
 
 function string(s) {
-	let result = [];
+	let result = "";
 	let split = s.split("");
 
 	for (let i = 0; i < split.length; i++) {
-		result.push(split[i]);
-		if (result === result.reverse() || result.length > 1) {
-			console.log(result);
-			console.log(result.reverse());
-			console.log(result === result.reverse());
-			break;
+		result += split[i];
+		if (result === result.split("").reverse().join("") && result.length > 1) {
+			return result;
 		}
 	}
 }
 
-string("babad");
+console.log(string("babad"));
