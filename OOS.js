98a2249207d4c83b2e5fbc767cf44e8a49623775
Aventom <ravaneloe@gmail.com>
2023-02-08 22:00:12 +0700

    nyoba

--- OOS.js
+++ OOS.js
@@ -1,148 +1,158 @@
-// let data = {
-// 	companyName: "Jojonomic",
-// 	numberOfEmployee: 100,
-// 	receiveNewEmployee: true,
-// 	location: {
-// 		address: "jakarta jalan nangka",
-// 		provinsi: "Dki",
+// // let data = {
+// // 	companyName: "Jojonomic",
+// // 	numberOfEmployee: 100,
+// // 	receiveNewEmployee: true,
+// // 	location: {
+// // 		address: "jakarta jalan nangka",
+// // 		provinsi: "Dki",
+// // 	},
+// // 	employeList: [
+// // 		{ name: "Employee 1", age: 25 },
+// // 		{ name: "Employee 2", age: 27 },
+// // 	],
+// // };
+
+// // console.log(data.companyName);
+// // console.log(data.numberOfEmployee);
+// // console.log(data.location.provinsi);
+// // console.log(data.employeList[1].age);
+
+// // let table = [
+
+// //         {
+// //             Name: "Employee1",
+// //             Gender : "Male",
+// //             Age : 20,
+// //             Permanen : true,
+// //         },
+// //         {
+// //             Name: "Employee2",
+// //             Gender : "Female",
+// //             Age : 25,
+// //             Permanen : false,
+// //         },
+// //         {
+// //             Name: "Employee3",
+// //             Gender : "Male",
+// //             Age : 33,
+// //             Permanen : true,
+// //         },
+
+// // ]
+
+// // let json = JSON.stringify(table)
+
+// // fetch(json)
+// // .then(response => response.json())
+// // .then(data => showTable(data))
+
+// // function showTable(data){
+// //     console.log(data)
+// // }
+
+// let produk = [
+// 	{
+// 		Name: "Employee1",
+// 		Gender: "Male",
+// 		Age: 20,
+// 		Permanen: true,
+// 		itemType: "Food",
 // 	},
-// 	employeList: [
-// 		{ name: "Employee 1", age: 25 },
-// 		{ name: "Employee 2", age: 27 },
+// 	{
+// 		Name: "Employee2",
+// 		Gender: "Female",
+// 		Age: 25,
+// 		Permanen: false,
+// 		itemType: "Drink",
+// 	},
+// 	{
+// 		Name: "Employee3",
+// 		Gender: "Male",
+// 		Age: 33,
+// 		Permanen: true,
+// 		itemType: "Tools",
+// 	},
+// 	{
+// 		Name: "Employee3",
+// 		Gender: "Male",
+// 		Age: 33,
+// 		Permanen: true,
+// 		itemType: "Cleaner",
+// 	},
+// ];
+
+// let value = Object.values(produk);
+
+// // function pemisahBasket(item){
+// //     let organicBasket = []
+// //     let otherBasket = []
+
+// //     for(let i = 0; i < item.length; i++){
+// //         if(item[i].itemType === "Food" || item[i].itemType === "Drink"){
+// //             organicBasket.push(item[i].itemType)
+// //         }
+// //         else if(item[i].itemType === "Tools" || item[i].itemType === "Cleaner"){
+// //             otherBasket.push(item[i].itemType)
+// //         }
+// //     }
+// //     return `${organicBasket} ${otherBasket}`
+// // }
+
+// // console.log(pemisahBasket(value))
+
+// const fileJson = {
+// 	shopingList: [
+// 		{
+// 			id: 1,
+// 			Item: "IDM Ramos Super 5KG",
+// 			Price: 59900,
+// 			Discount: 4000,
+// 			Value: 1,
+// 		},
+// 		{
+// 			id: 2,
+// 			Item: "So Klin Softergen 800",
+// 			Price: 14700,
+// 			Discount: 2200,
+// 			Value: 1,
+// 		},
+// 		{
+// 			id: 3,
+// 			Item: "Mogu Mogu Lyche 320",
+// 			Price: 59900,
+// 			Discount: 0,
+// 			Value: 1,
+// 		},
+// 		{
+// 			id: 4,
+// 			Item: "Plastik BSR",
+// 			Price: 59900,
+// 			Discount: 0,
+// 			Value: 3,
+// 		},
 // 	],
 // };
+// let filter = fileJson.shopingList.filter((items) => items.Discount !== 0);
 
-// console.log(data.companyName);
-// console.log(data.numberOfEmployee);
-// console.log(data.location.provinsi);
-// console.log(data.employeList[1].age);
+// let objectName;
+// let companyName;
+// let employeeOne;
+// let employeeTwoo;
+// let gender;
+// let male;
+// let female;
+// let jSon_1;
+// let jSon_2;
+// let jSon_3;
 
-// let table = [
+// console.log(filter);
 
-//         {
-//             Name: "Employee1",
-//             Gender : "Male",
-//             Age : 20,
-//             Permanen : true,
-//         },
-//         {
-//             Name: "Employee2",
-//             Gender : "Female",
-//             Age : 25,
-//             Permanen : false,
-//         },
-//         {
-//             Name: "Employee3",
-//             Gender : "Male",
-//             Age : 33,
-//             Permanen : true,
-//         },
+function oos(n) {
+	for (let i = 1; i <= n; i++) {
+		for (let j = 1; j <= 3; j++) {
+			console.log(i);
+		}
+	}
+}
 
-// ]
-
-// let json = JSON.stringify(table)
-
-// fetch(json)
-// .then(response => response.json())
-// .then(data => showTable(data))
-
-// function showTable(data){
-//     console.log(data)
-// }
-
-let produk = [
-	{
-		Name: "Employee1",
-		Gender: "Male",
-		Age: 20,
-		Permanen: true,
-		itemType: "Food",
-	},
-	{
-		Name: "Employee2",
-		Gender: "Female",
-		Age: 25,
-		Permanen: false,
-		itemType: "Drink",
-	},
-	{
-		Name: "Employee3",
-		Gender: "Male",
-		Age: 33,
-		Permanen: true,
-		itemType: "Tools",
-	},
-	{
-		Name: "Employee3",
-		Gender: "Male",
-		Age: 33,
-		Permanen: true,
-		itemType: "Cleaner",
-	},
-];
-
-let value = Object.values(produk);
-
-// function pemisahBasket(item){
-//     let organicBasket = []
-//     let otherBasket = []
-
-//     for(let i = 0; i < item.length; i++){
-//         if(item[i].itemType === "Food" || item[i].itemType === "Drink"){
-//             organicBasket.push(item[i].itemType)
-//         }
-//         else if(item[i].itemType === "Tools" || item[i].itemType === "Cleaner"){
-//             otherBasket.push(item[i].itemType)
-//         }
-//     }
-//     return `${organicBasket} ${otherBasket}`
-// }
-
-// console.log(pemisahBasket(value))
-
-const fileJson = {
-	shopingList: [
-		{
-			id: 1,
-			Item: "IDM Ramos Super 5KG",
-			Price: 59900,
-			Discount: 4000,
-			Value: 1,
-		},
-		{
-			id: 2,
-			Item: "So Klin Softergen 800",
-			Price: 14700,
-			Discount: 2200,
-			Value: 1,
-		},
-		{
-			id: 3,
-			Item: "Mogu Mogu Lyche 320",
-			Price: 59900,
-			Discount: 0,
-			Value: 1,
-		},
-		{
-			id: 4,
-			Item: "Plastik BSR",
-			Price: 59900,
-			Discount: 0,
-			Value: 3,
-		},
-	],
-};
-let filter = fileJson.shopingList.filter((items) => items.Discount !== 0);
-
-let objectName;
-let companyName;
-let employeeOne;
-let employeeTwoo;
-let gender;
-let male;
-let female;
-let jSon_1;
-let jSon_2;
-let jSon_3;
-
-console.log(filter);
+oos(3);
