96c2abcf1fedc10a1e51241e867c394c88e49b99
Aventom <ravaneloe@gmail.com>
2023-02-08 14:55:52 +0700

    function

--- binarySearch.js
+++ binarySearch.js
@@ -1,4 +1,21 @@
-let a = ["a", "b"];
-let b = ["b", "a"];
+// let a = ["a", "b"];
+// let b = ["b", "a"];
 
-console.log(a === b);
+// console.log(a === b);
+
+function string(s) {
+	let result = [];
+	let split = s.split("");
+
+	for (let i = 0; i < split.length; i++) {
+		result.push(split[i]);
+		if (result === result.reverse() || result.length > 1) {
+			console.log(result);
+			console.log(result.reverse());
+			console.log(result === result.reverse());
+			break;
+		}
+	}
+}
+
+string("babad");
