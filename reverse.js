28f861d9051e5fd6ba18a687261c337dcd0367b9
Aventom <ravaneloe@gmail.com>
2023-02-08 16:27:36 +0700

    reversessss

---
+++ reverse.js
@@ -0,0 +1,9 @@
+function reverse(arr) {
+	let split = arr.toString().split("");
+	let result = split.splice(0, 1).concat();
+	let reverse = result.reverse();
+	console.log(reverse);
+	console.log(reverse);
+}
+
+reverse(-321);
