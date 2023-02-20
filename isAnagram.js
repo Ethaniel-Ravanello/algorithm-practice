0c4ebfca9b19e5f0fdac0e26d1548c898928bde7
Ethaniel-Ravanello <ravaneloe@gmail.com>
2023-02-20 09:08:14 +0700

    Testing

--- isAnagram.js
+++ isAnagram.js
@@ -1,12 +1,12 @@
 function isAnagram(s, t) {
-	s = s.split("").sort().join();
-	t = t.split("").sort().join();
-
-	if (s === t) {
-		return true;
-	} else {
-		return false;
-	}
+  s = s.split("").sort().join();
+  t = t.split("").sort().join();
+  let abc;
+  if (s === t) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(isAnagram);
