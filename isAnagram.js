78a7bd191c6bb26e34ddfe46b42715fd6ed5eb59
Ethaniel-Ravanello <ravaneloe@gmail.com>
2023-03-05 15:32:53 +0700

    Heh

--- isAnagram.js
+++ isAnagram.js
@@ -2,10 +2,6 @@ function isAnagram(s, t) {
   s = s.split("").sort().join();
   t = t.split("").sort().join();
   let abcdefg;
-  let testing;
-  let testing2;
-  let testing3;
-  let testing4;
   if (s === t) {
     return true;
   } else {
